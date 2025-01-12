336ef4e826ab9d2a688bdeff2df9836ba0ad5260
MelihCelik00 <melihsafa.c@gmail.com>
2025-01-12 18:57:41 +0300

    added postgresql support for tests

--- src/test/java/com/oms/OrderManagementSystemApplicationTests.java
+++ src/test/java/com/oms/OrderManagementSystemApplicationTests.java
@@ -1,10 +1,11 @@
 package com.oms;
 
+import com.oms.config.PostgresTestContainer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OrderManagementSystemApplicationTests {
+class OrderManagementSystemApplicationTests extends PostgresTestContainer {
 
 	@Test
 	void contextLoads() {
